9163ad106c6b313c947b8ac33fbde601e3b74da6
Viktoria Shirunova <vvizvarina@gmail.com>
2020-10-31 15:48:00 +0300

    init project

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.tsx",
+  devtool: "source-map",
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx", ".json"],
+  },
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "index.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|ts)x?$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  devServer: {
+    historyApiFallback: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+};
