4eec30d2d4daed8c9fe6897e2a702c03a18574f0
Viktoria Shirunova <vv16vv@users.noreply.github.com>
2020-11-03 15:08:25 +0300

    [hw03] easy3: corrected solution - use both type parameters

--- src/hw03/easy3.ts
+++ src/hw03/easy3.ts
@@ -3,7 +3,7 @@
 // Нужно заменить FIXME на соответствующий тип
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-type FIXME<T extends Record<any, any>, K extends keyof T> = Omit<T, keyof T>;
+type FIXME<T extends Record<any, any>, K extends keyof T> = Omit<T, K>;
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const omit = <T extends Record<any, any>, K extends keyof T>(
