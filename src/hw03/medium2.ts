dc6b8e3912914dc809999ded1539799ed5b9589a
Viktoria Shirunova <vv16vv@users.noreply.github.com>
2020-11-06 11:24:42 +0300

    [hw03] medium2 - solution with infer

--- src/hw03/medium2.ts
+++ src/hw03/medium2.ts
@@ -3,11 +3,11 @@
 // Нужно заменить FIXME на правильный тип
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-type FIXME<T> = Partial<T> | undefined;
+type FIXME<T> = T extends { defaultProps: infer P } ? P : never;
 
 // Hint: infer
 export const getDefaultProps = <T>(
   component: React.ComponentType<T>
 ): FIXME<T> => {
-  return component.defaultProps;
+    return component.defaultProps as FIXME<T>;
 };
