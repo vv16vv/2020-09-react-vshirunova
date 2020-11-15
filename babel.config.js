119104cf2092194b09f2d6b1d48946eb01972c59
Viktoria Shirunova <vv16vv@users.noreply.github.com>
2020-11-15 20:14:26 +0300

    [hw04] workaround allowing to have arrow functions in class
    taken from https://github.com/nrwl/nx/issues/1779#issuecomment-668544535

--- babel.config.js
+++ babel.config.js
@@ -4,4 +4,8 @@ module.exports = {
         "@babel/preset-react",
         "@babel/preset-typescript",
     ],
+    "plugins": [
+        ["@babel/plugin-proposal-decorators", { "legacy": true }],
+        ["@babel/plugin-proposal-class-properties", { "loose" : true }]
+    ],
 };
