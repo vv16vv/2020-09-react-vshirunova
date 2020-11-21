0320831c4c63ebc313c6d6f3ced7e7dcb221eb32
Viktoria Shirunova <vv16vv@users.noreply.github.com>
2020-11-21 12:40:33 +0300

    [hw05] added theme

---
+++ src/components/StyledColors.tsx
@@ -0,0 +1,16 @@
+
+const LightTheme = {
+    backgroundColor: '#ffffff',
+    backForeColor: '#888888',
+    mainForeColor: '#000000',
+    accentForeColor: '#152ea5'
+}
+
+const DarkTheme = {
+    backgroundColor: '#000000',
+    backForeColor: '#e5dede',
+    mainForeColor: '#f7eec1',
+    accentForeColor: '#ffae2c'
+}
+
+export const CurrentTheme = LightTheme;
