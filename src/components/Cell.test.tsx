ec0c8e54f426298e7613b3b13e0bb27d4733d137
Viktoria Shirunova <vv16vv@users.noreply.github.com>
2020-11-27 17:42:54 +0300

    [hw05] removed snapshot tests

--- src/components/Cell.test.tsx
+++ src/components/Cell.test.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import renderer from "react-test-renderer";
 import {shallow} from "enzyme";
 import {Cell} from "./Cell";
 
@@ -8,28 +7,6 @@ describe("Cell", () => {
     const clickHandler = jest.fn();
     const x = 3;
     const y = 4;
-
-    it("should show coordinates and filled up sign", () => {
-        expect(renderer.create(
-            <Cell
-                x={x}
-                y={y}
-                filled={true}
-                clickHandler={clickHandler}/>
-            ).toJSON()
-        ).toMatchSnapshot();
-    });
-
-    it("should show coordinates and empty sign", () => {
-        expect(renderer.create(
-            <Cell
-                x={x}
-                y={y}
-                filled={false}
-                clickHandler={clickHandler}/>
-            ).toJSON()
-        ).toMatchSnapshot();
-    });
 
     it('should pass coordinates to the onClick handler', () => {
 
--- src/components/Field.test.tsx
+++ src/components/Field.test.tsx
@@ -20,40 +20,6 @@ describe("Field", () => {
         }
     });
 
-    it("table 1x1 with filled cell", () => {
-        expect(renderer.create(
-            <Field
-                width={1}
-                height={1}
-                filledCells={[{x: 1, y: 1}]}
-                clickHandler={clickHandler}/>
-            ).toJSON()
-        ).toMatchSnapshot();
-    });
-
-    it("table 1x1 with empty cell", () => {
-        expect(renderer.create(
-            <Field
-                width={1}
-                height={1}
-                filledCells={[]}
-                clickHandler={clickHandler}/>
-            ).toJSON()
-        ).toMatchSnapshot();
-    });
-
-    it("table 3x4 with different cells", () => {
-        expect(renderer.create(
-            <Field
-                width={3}
-                height={4}
-                filledCells={filledCellsData}
-                clickHandler={clickHandler}/>
-            ).toJSON()
-        ).toMatchSnapshot();
-    });
-
-
     it('table should pass onClick handler to nested cells', () => {
         const wrapper = mount(
             <Field width={2} height={2} filledCells={filledCellsData} clickHandler={clickHandler}/>
