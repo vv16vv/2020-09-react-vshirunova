d58bafd9e3bfc6abed951329ec136dd5b82e81dc
Viktoria Shirunova <vv16vv@users.noreply.github.com>
2020-12-02 22:41:56 +0300

    [hw06] pureFunctions.ts - solution

--- src/hw06/pureFunctions.ts
+++ src/hw06/pureFunctions.ts
@@ -2,18 +2,29 @@
 export type Team = { name: string; score: number };
 
 export const getTopName = (teams: Team[]): string => {
-  //
+    return teams.sort((a, b) => (a.score === b.score) ? 0 : (a.score > b.score) ? 1 : -1)
+        ?.pop()
+        ?.name ?? "";
 };
 
 // Задание 2
 export type QsObj = Record<string, string | number | boolean | object>;
 
 export const createQs = (qsObj: QsObj): string => {
-  //
+    return "?" + Object.entries(qsObj)
+        .map(([k, v]) => `${k}=${v}`)
+        .join('&');
 };
 
 // Задание 3
 
 export const parseQs = (qs: string): QsObj => {
-  //
+    return qs
+        .substring(1) // remove first '?'
+        .split("&")
+        .map((param) => param.split("="))
+        .reduce((acc, [k, v]) => {
+            acc[k] = v
+            return acc;
+        }, {}) as QsObj
 };
