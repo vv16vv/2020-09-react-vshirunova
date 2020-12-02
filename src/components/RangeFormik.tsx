db81dc73f28312f27634cb9453b4f59a527f5a11
Viktoria Shirunova <vv16vv@users.noreply.github.com>
2020-12-02 14:54:50 +0300

    [hw05] Range -> RangeFormik to be used in SettingsFormikForm

---
+++ src/components/RangeFormik.tsx
@@ -0,0 +1,41 @@
+import React, {ChangeEvent, useCallback, useState} from "react";
+import {RangeBlock, StyledRange} from "./StyledComponents";
+import {CenteredLabel, RangeLabel} from "./StyledTextComponents";
+import {Field} from "formik";
+
+export interface RangeProp {
+    name: string;
+    min?: number;
+    max: number;
+    step?: number;
+    value: number;
+    changeHandler: (newValue: number) => void;
+}
+
+export const RangeFormik: React.FC<RangeProp> = ({name, min, max, step, value, changeHandler}) => {
+    const realMin = min ?? 1;
+    const [currentValue, setCurValue] = useState(value);
+
+    const onChange = useCallback((event: ChangeEvent<HTMLInputElement>) => {
+        const newValue = +event?.target?.value ?? realMin;
+        changeHandler(newValue);
+        setCurValue(newValue);
+    }, []);
+
+    return (<RangeBlock>
+        <div><CenteredLabel>{currentValue}</CenteredLabel></div>
+        <div>
+            <RangeLabel>{realMin}</RangeLabel>
+            <Field name={name}
+                   type="range"
+                   min={realMin}
+                   max={max}
+                   step={step ?? 1}
+                   value={currentValue}
+                   onChange={onChange}
+                   component={StyledRange}
+            />
+            <RangeLabel>{max}</RangeLabel>
+        </div>
+    </RangeBlock>)
+};
