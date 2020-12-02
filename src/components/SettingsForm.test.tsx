1d61498dc3bdfb52f88c929aadbbe2788e326f9b
Viktoria Shirunova <vv16vv@users.noreply.github.com>
2020-12-02 14:19:32 +0300

    [hw05] attempt to fix test for checking onSubmit on Formik form (unsuccessful)

--- src/components/SettingsForm.test.tsx
+++ src/components/SettingsForm.test.tsx
@@ -2,19 +2,22 @@ import 'jsdom-global/register';
 import React from "react";
 import {SettingsForm} from "./SettingsForm";
 import {mount} from "enzyme";
+import {act} from "@testing-library/react";
 
 describe("Settings form", () => {
 
     const submitHandler = jest.fn();
 
-    it("should process submit", () => {
+    it("should process submit", async () => {
         const wrapper = mount(
             <SettingsForm onSubmit={submitHandler}/>
         );
-        wrapper
-            .find(SettingsForm)
-            .simulate('submit')
-        expect(submitHandler).toBeCalled()
+        act(() => {
+            wrapper
+                .find('button')
+                .simulate('submit');
+        });
+        await expect(submitHandler).toHaveBeenCalled()
     });
 
 });
