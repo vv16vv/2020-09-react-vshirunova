3005597595e4cc65bb9af8e47ee16e6191cbea8c
Viktoria Shirunova <vv16vv@users.noreply.github.com>
2020-12-02 14:19:32 +0300

    [hw05] attempt to fix test for checking onSubmit on Formik form (unsuccessful)

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,11 @@
 module.exports = {
     presets: [
-        "@babel/preset-env",
+        ["@babel/preset-env",
+        {
+            targets: {
+                esmodules: true,
+            },
+        }],
         "@babel/preset-react",
         "@babel/preset-typescript",
     ],
