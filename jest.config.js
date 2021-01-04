6df65ea68c236c3fb4ae655408185ec758180204
Viktoria Shirunova <vv16vv@users.noreply.github.com>
2021-01-04 22:11:17 +0300

    [hw08] make jest work with import aliases

--- jest.config.js
+++ jest.config.js
@@ -10,4 +10,9 @@ module.exports = {
   transform: {
     "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest",
   },
+  moduleNameMapper: {
+    "^styled/(.*)$": "<rootDir>/src/components/styled/$1",
+    "^cmp/(.*)$": "<rootDir>/src/components/$1",
+    "^@/(.*)$": "<rootDir>/src/$1",
+  }
 };
