44502849ae63a371812093198dbb57d0021e2efc
Viktoria Shirunova <vv16vv@users.noreply.github.com>
2021-01-04 23:41:21 +0300

    [hw08] added test for NotFoundPage

---
+++ src/components/NotFoundPage/NotFoundPage.test.tsx
@@ -0,0 +1,23 @@
+import 'jsdom-global/register';
+import React from "react";
+import {mount} from "enzyme";
+import {NotFound} from "./NotFoundPage";
+
+describe("NotFound component", () => {
+
+    const clickHandler = jest.fn();
+    const notFoundPath = "notFound";
+
+
+    it('should pass onClick handler', () => {
+        const wrapper = mount(
+            <NotFound notFoundPath={notFoundPath} onBack={clickHandler}/>
+        );
+
+        wrapper
+            .find("button")
+            .simulate('click');
+
+        expect(clickHandler).toBeCalled();
+    });
+});
