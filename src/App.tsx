f9867e4ee7b406ac2658dcc83197f1afec67fb80
Viktoria Shirunova <vv16vv@users.noreply.github.com>
2021-01-05 12:30:06 +0300

    [hw08] App: added router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import {GameLayout} from "cmp/GameLayout";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import {NotFoundPage} from "cmp/NotFoundPage";
+import {LoginFormPage} from "cmp/LoginForm";
+import {Paths} from "@/Paths";
 
-function App() {
-  return (
-    <GameLayout/>
-  );
+export const App: React.FC<{}> = () => {
+    return (
+        <Router>
+            <Switch>
+                <Route exact path={Paths.Root} component={LoginFormPage}/>
+                <Route path={Paths.Game} component={GameLayout}/>
+                <Route component={NotFoundPage}/>
+            </Switch>
+        </Router>
+    );
 }
 
-export default App;
