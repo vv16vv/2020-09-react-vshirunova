4ae0540b1ec0c1ad32001f1c9a0a0c937accc299
Viktoria Shirunova <vv16vv@users.noreply.github.com>
2021-01-08 11:26:51 +0300

    [hw08] extract NotFoundPage to the separate folder

---
+++ src/pages/NotFound/NotFoundPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import {useHistory, useLocation} from "react-router-dom";
+import {Paths} from "@/Paths";
+import {NotFound} from "cmp/NotFound/NotFound";
+
+export const NotFoundPage: React.FC<{}> = () => {
+    const {pathname} = useLocation()
+    const history = useHistory()
+    return (
+        <NotFound notFoundPath={pathname} onBack={() => history.push(Paths.Root)}/>
+    )
+}
