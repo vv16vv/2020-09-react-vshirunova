8ef6e5ed5f4b4cad19f315ea6ccd096ccb4b4d15
Viktoria Shirunova <vv16vv@users.noreply.github.com>
2021-01-08 12:48:58 +0300

    [hw08] extract GamePage to the separate folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
-import {GameLayout} from "cmp/GameLayout";
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+
 import {Paths} from "@/Paths";
 import {NotFoundPage} from "@/pages/NotFound";
 import {LoginPage} from "@/pages/Login";
+import {GamePage} from "@/pages/Game";
 
 export const App: React.FC<{}> = () => {
     return (
         <Router>
             <Switch>
                 <Route exact path={Paths.Root} component={LoginPage}/>
-                <Route path={Paths.Game} component={GameLayout}/>
+                <Route path={Paths.Game} component={GamePage}/>
                 <Route component={NotFoundPage}/>
             </Switch>
         </Router>
