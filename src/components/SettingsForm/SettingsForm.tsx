4fdfdbf81042828f0aa7f0a2cf990c85634bb096
Viktoria Shirunova <vv16vv@users.noreply.github.com>
2021-01-19 12:48:11 +0300

    [hw09] used redux for SettingsForm

--- src/components/SettingsForm/SettingsForm.tsx
+++ src/components/SettingsForm/SettingsForm.tsx
@@ -1,92 +1,115 @@
-import React from "react";
+import React, {useCallback, useState} from "react";
+import {Dispatch} from "redux";
+import {connect} from "react-redux";
+
 import {Range} from "cmp/Range";
 import {FieldTitle, TableTitle} from "styled/StyledTextComponents";
 import {LayoutTable, LayoutTd, LayoutTr, StyledButton, StyledFieldSet} from "styled/StyledComponents";
+import {settingsSubmit, SettingsSubmitPayload} from "@/rdx/features/settings";
+import {AppState} from "@/rdx/reducers";
 
-export interface SettingsFormResult {
+interface ReduxProps {
     width: number;
     height: number;
     frequency: number;
     userName?: string;
-}
-
-export interface SettingsFormProps extends SettingsFormResult {
-    onSubmit: (settings: SettingsFormResult) => void;
+    onSubmit: (settings: SettingsSubmitPayload) => void;
 }
 
 const milliInSecond = 1000;
 
-export class SettingsForm extends React.Component<SettingsFormProps, SettingsFormResult> {
-
-    constructor(props: SettingsFormProps) {
-        super(props);
-        this.state = {
-            ...props,
-            userName: this.calculateUserName(props.userName),
-            frequency: this.calculateFrequency(props.frequency)
-        };
-    }
-
-    changeValue = (field: string, newValue: number) => {
-        this.setState({
-            [field]: newValue
-        } as Pick<SettingsFormResult, "width" | "height" | "frequency">)
-    }
-
-    handleSubmit = (event: React.FormEvent) => {
-        event.preventDefault();
-
-        this.props.onSubmit({
-            width: this.state.width,
-            height: this.state.height,
-            frequency: this.state.frequency * milliInSecond,
-        });
-    }
-
-    calculateUserName = (userName: string | undefined): string => {
-        return userName && userName !== "" ? `for ${userName}` : ""
-    }
-
-    calculateFrequency = (frequency: number):number => {
-        return frequency / milliInSecond
-    }
-
-    render() {
-        return (
-            <form onSubmit={this.handleSubmit}>
-                <StyledFieldSet>
-                    <TableTitle>{`Game Settings ${this.state.userName}`}</TableTitle>
-                    <LayoutTable>
-                        <tbody>
-                        <LayoutTr>
-                            <LayoutTd><FieldTitle>Width</FieldTitle></LayoutTd>
-                            <LayoutTd><Range
-                                name={"width"}
-                                max={50}
-                                value={this.state.width}
-                                changeHandler={this.changeValue}/></LayoutTd>
-                        </LayoutTr>
-                        <LayoutTr>
-                            <LayoutTd><FieldTitle>Height</FieldTitle></LayoutTd>
-                            <LayoutTd><Range
-                                name={"height"}
-                                max={50}
-                                value={this.state.height}
-                                changeHandler={this.changeValue}/></LayoutTd>
-                        </LayoutTr>
-                        <LayoutTr>
-                            <LayoutTd><FieldTitle>Frequency, sec</FieldTitle></LayoutTd>
-                            <LayoutTd><Range
-                                name={"frequency"}
-                                max={10} min={2} step={0.5}
-                                value={this.state.frequency}
-                                changeHandler={this.changeValue}/></LayoutTd>
-                        </LayoutTr>
-                        </tbody>
-                    </LayoutTable>
-                </StyledFieldSet>
-                <StyledButton>Apply</StyledButton>
-            </form>
-        );
-    }
+const calculateTitle = (userName: string | undefined): string => {
+    return userName && userName !== "" ? `for ${userName}` : ""
 }
+
+const calculateFrequency = (frequency: number): number => {
+    return frequency / milliInSecond
+}
+
+const RawSettingsForm: React.FC<ReduxProps> = props => {
+    const [width, setWidth] = useState(props.width)
+    const [height, setHeight] = useState(props.height)
+    const [frequency, setFrequency] = useState(calculateFrequency(props.frequency))
+    const [title] = useState(calculateTitle(props.userName))
+
+    const changeValue = useCallback((field: string, newValue: number) => {
+        switch (field) {
+            case "width": {
+                setWidth(newValue);
+                break
+            }
+            case "height": {
+                setHeight(newValue);
+                break
+            }
+            case "frequency": {
+                setFrequency(newValue);
+                break
+            }
+        }
+    }, [width, height, frequency])
+
+    const handleSubmit = useCallback((event: React.FormEvent) => {
+        event.preventDefault();
+        props.onSubmit({
+            width: width,
+            height: height,
+            frequency: frequency * milliInSecond,
+        });
+    }, [width, height, frequency])
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <StyledFieldSet>
+                <TableTitle>{`Game Settings ${title}`}</TableTitle>
+                <LayoutTable>
+                    <tbody>
+                    <LayoutTr>
+                        <LayoutTd><FieldTitle>Width</FieldTitle></LayoutTd>
+                        <LayoutTd><Range
+                            name={"width"}
+                            max={50}
+                            value={width}
+                            changeHandler={changeValue}/></LayoutTd>
+                    </LayoutTr>
+                    <LayoutTr>
+                        <LayoutTd><FieldTitle>Height</FieldTitle></LayoutTd>
+                        <LayoutTd><Range
+                            name={"height"}
+                            max={50}
+                            value={height}
+                            changeHandler={changeValue}/></LayoutTd>
+                    </LayoutTr>
+                    <LayoutTr>
+                        <LayoutTd><FieldTitle>Frequency, sec</FieldTitle></LayoutTd>
+                        <LayoutTd><Range
+                            name={"frequency"}
+                            max={10} min={2} step={0.5}
+                            value={frequency}
+                            changeHandler={changeValue}/></LayoutTd>
+                    </LayoutTr>
+                    </tbody>
+                </LayoutTable>
+            </StyledFieldSet>
+            <StyledButton>Apply</StyledButton>
+        </form>
+    );
+}
+
+
+function mapStateToProps(state: AppState) {
+    return {
+        width: state.settingsReducer.width,
+        height: state.settingsReducer.height,
+        frequency: state.settingsReducer.frequency,
+        userName: state.loginReducer.user
+    };
+}
+
+function mapDispatchToProps(dispatch: Dispatch) {
+    return {
+        onSubmit: (settings: SettingsSubmitPayload) => dispatch(settingsSubmit(settings)),
+    };
+}
+
+export const SettingsForm = connect(mapStateToProps, mapDispatchToProps)(RawSettingsForm);
--- src/rdx/reducers.ts
+++ src/rdx/reducers.ts
@@ -2,10 +2,12 @@ import {combineReducers} from "redux";
 
 import {gameReducer} from "@/rdx/features/game";
 import {loginReducer} from "@/rdx/features/login";
+import {settingsReducer} from "@/rdx/features/settings";
 
 export const rootReducer = combineReducers({
     loginReducer,
-    gameReducer
+    gameReducer,
+    settingsReducer,
 })
 
 export type AppState = ReturnType<typeof rootReducer>
