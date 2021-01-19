a3eaa0724227308a69e28d4795fb57a325a27222
Viktoria Shirunova <vv16vv@users.noreply.github.com>
2021-01-19 16:32:42 +0300

    [hw09] used redux for LogoutButton, simplified GameWithSettings

---
+++ src/components/GameWithSettings/LogoutButton.tsx
@@ -0,0 +1,34 @@
+import React, {useCallback} from "react";
+import {useHistory} from "react-router-dom";
+import {Dispatch} from "redux";
+import {connect} from "react-redux";
+
+import {Paths} from "@/Paths";
+import {StyledButton} from "styled/StyledComponents";
+import {logout} from "@/rdx/features/login";
+import {AppState} from "@/rdx/reducers";
+
+interface ReduxProps {
+    onLogout: () => void
+}
+
+const RawLogoutButton: React.FC<ReduxProps> = props => {
+    const history = useHistory()
+    const logout = useCallback(() => {
+        props.onLogout()
+        history.push(Paths.Root)
+    }, [])
+    return <StyledButton onClick={logout}>Log out</StyledButton>
+}
+
+function mapStateToProps(state: AppState) {
+    return {};
+}
+
+function mapDispatchToProps(dispatch: Dispatch) {
+    return {
+        onLogout: () => dispatch(logout()),
+    };
+}
+
+export const LogoutButton = connect(mapStateToProps, mapDispatchToProps)(RawLogoutButton);
