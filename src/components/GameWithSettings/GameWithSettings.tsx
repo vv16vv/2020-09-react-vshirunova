81d3a61c07cb95c4ca33069edb4a113f656ab550
Viktoria Shirunova <vv16vv@users.noreply.github.com>
2021-01-19 16:32:42 +0300

    [hw09] used redux for LogoutButton, simplified GameWithSettings

--- src/components/GameWithSettings/GameWithSettings.tsx
+++ src/components/GameWithSettings/GameWithSettings.tsx
@@ -1,60 +1,14 @@
-import React, {useCallback, useEffect, useState} from "react";
-import {SettingsForm, SettingsFormResult} from "cmp/SettingsForm";
+import React from "react";
+
+import {SettingsForm} from "cmp/SettingsForm";
 import {JumpingCellGame} from "cmp/JumpingCellGame";
-import {LoginStorage} from "@/logic/LoginStorage";
-import {useHistory} from "react-router-dom";
-import {Paths} from "@/Paths";
-import {StyledButton} from "styled/StyledComponents";
-
-export const initialGameSettings: SettingsFormResult = {
-    width: 10,
-    height: 10,
-    frequency: 5000
-} as const;
-
-const LogoutButton: React.FC<{}> = () => {
-    const history = useHistory()
-    const logout = useCallback(() => {
-        LoginStorage
-            .clearName()
-            .then(() => history.push(Paths.Root))
-    }, [])
-    return <StyledButton onClick={logout}>Log out</StyledButton>
-}
+import {LogoutButton} from "cmp/GameWithSettings/LogoutButton";
 
 export const GameWithSettings: React.FC<{}> = (() => {
-    const [width, setWidth] = useState(initialGameSettings.width);
-    const [height, setHeight] = useState(initialGameSettings.height);
-    const [frequency, setFrequency] = useState(initialGameSettings.frequency);
-    const [userName, setUserName] = useState("");
-
-    useEffect(() => {
-        const getLogin = () => {
-            LoginStorage
-                .getCurrentName()
-                .then(login => {
-                    setUserName(login)
-                })
-        }
-        getLogin()
-    }, [userName])
-
-    const handleSubmit = useCallback(({width, height, frequency}: SettingsFormResult) => {
-        console.log(`handleSubmit: new width=${width}, height=${height}, frequency=${frequency}`)
-        setWidth(width);
-        setHeight(height);
-        setFrequency(frequency);
-    }, [width, height, frequency])
-
     return (
         <>
-            <SettingsForm
-                width={width}
-                height={height}
-                frequency={frequency}
-                userName={userName}
-                onSubmit={handleSubmit}/>
-            <JumpingCellGame width={width} height={height} frequency={frequency}/>
+            <SettingsForm/>
+            <JumpingCellGame/>
             <LogoutButton/>
         </>
     );
