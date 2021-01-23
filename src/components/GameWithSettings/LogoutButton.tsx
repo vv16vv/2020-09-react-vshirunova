b17f3a7e3f30c65b7f941e53ff42eb0b6acdd4ab
Viktoria Shirunova <vv16vv@users.noreply.github.com>
2021-01-23 12:48:00 +0300

    [hw09] moved route changes to corresponding thunks

--- src/components/GameWithSettings/LogoutButton.tsx
+++ src/components/GameWithSettings/LogoutButton.tsx
@@ -1,9 +1,6 @@
 import React, {useCallback} from "react";
-import {useHistory} from "react-router-dom";
 import {Dispatch} from "redux";
 import {connect} from "react-redux";
-
-import {Paths} from "@/Paths";
 import {StyledButton} from "@/components/styled/StyledComponents";
 import {clearName} from "@/rdx/features/login";
 import {AppState} from "@/rdx/reducers";
@@ -13,10 +10,8 @@ interface ReduxProps {
 }
 
 const RawLogoutButton: React.FC<ReduxProps> = props => {
-    const history = useHistory()
     const logout = useCallback(() => {
         props.onLogout()
-        history.push(Paths.Root)
     }, [])
     return <StyledButton onClick={logout}>Log out</StyledButton>
 }
