15f2f1886ceb1ef4bd97af566500e800d42fc555
Viktoria Shirunova <vv16vv@users.noreply.github.com>
2021-01-23 13:34:06 +0300

    [hw09] using bindActionCreators in thunks

--- src/pages/Game/GamePage.tsx
+++ src/pages/Game/GamePage.tsx
@@ -1,7 +1,7 @@
 import React, {useEffect} from "react";
 import {connect} from "react-redux";
 import {Redirect} from "react-router-dom";
-import {Dispatch} from "redux";
+import {bindActionCreators, Dispatch} from "redux";
 
 import {Paths} from "@/Paths";
 import {AppState} from "@/rdx/reducers";
@@ -16,7 +16,7 @@ interface ReduxProps {
 
 export const RawGamePage: React.FC<ReduxProps> = props => {
     useEffect(() => {
-        if(!props.isLoggingOut)
+        if (!props.isLoggingOut)
             props.loading()
     })
     return <>{
@@ -34,9 +34,9 @@ function mapStateToProps(state: AppState) {
 }
 
 function mapDispatchToProps(dispatch: Dispatch) {
-    return {
-        loading: () => dispatch(loading()),
-    };
+    return bindActionCreators({
+        loading
+    }, dispatch)
 }
 
 export const GamePage = connect(mapStateToProps, mapDispatchToProps)(RawGamePage);
