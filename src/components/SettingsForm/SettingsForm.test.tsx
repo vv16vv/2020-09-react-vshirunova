8c1ffc549aa366f2248f02b4114bca5bd7cc25ec
Viktoria Shirunova <vv16vv@users.noreply.github.com>
2021-01-26 15:13:18 +0300

    [hw09] fixed SettingsForm.test

--- src/components/SettingsForm/SettingsForm.test.tsx
+++ src/components/SettingsForm/SettingsForm.test.tsx
@@ -1,20 +1,30 @@
 import 'jsdom-global/register';
 import React from "react";
 import {mount} from "enzyme";
+import {Provider} from "react-redux";
 
 import {SettingsFormResult} from "@/components/SettingsForm/SettingsFormikForm";
 import {SettingsForm} from "@/components/SettingsForm/SettingsForm";
+import {defaultGameState} from "@/rdx/game/gameState";
+import {userLoggedIn} from "@/rdx/testConstants";
+import {mockStore} from "@/rdx/mockStore";
 
 describe("Settings form", () => {
+    let store: any;
+    beforeEach(async () => {
+        store = mockStore({
+            userReducer: userLoggedIn,
+            gameReducer: defaultGameState
+        })
+    })
 
-    const submitHandler = jest.fn();
-
-    const defaultValues:SettingsFormResult = {
+    const defaultValues: SettingsFormResult = {
         width: 10,
         height: 10,
         frequency: 2000
     };
-    const changedValues:SettingsFormResult = {
+
+    const changedValues: SettingsFormResult = {
         width: 15,
         height: 8,
         frequency: 1000
@@ -22,45 +32,47 @@ describe("Settings form", () => {
 
     it("should process submit with default values", () => {
         const wrapper = mount(
-            <SettingsForm
-              width={defaultValues.width}
-              height={defaultValues.height}
-              frequency={defaultValues.frequency}
-              onSubmit={submitHandler}/>
+            <Provider store={store}>
+                <SettingsForm/>
+            </Provider>
         );
         wrapper
             .find('button')
             .simulate('submit');
-        expect(submitHandler).toHaveBeenCalledWith(defaultValues)
+        expect(store.getActions()).toEqual([{
+            type: 'gameStart',
+            payload: defaultValues
+        }])
     });
 
     it("should process submit with changed values", () => {
         const wrapper = mount(
-          <SettingsForm
-            width={defaultValues.width}
-            height={defaultValues.height}
-            frequency={defaultValues.frequency}
-            onSubmit={submitHandler}/>
+            <Provider store={store}>
+                <SettingsForm/>
+            </Provider>
         );
         wrapper
             .find('input[name="width"]')
             .simulate("change", {
-                target: { value: changedValues.width },
+                target: {value: changedValues.width},
             });
         wrapper
             .find('input[name="height"]')
             .simulate("change", {
-                target: { value: changedValues.height },
+                target: {value: changedValues.height},
             });
         wrapper
             .find('input[name="frequency"]')
             .simulate("change", {
-                target: { value: changedValues.frequency / 1000 },
+                target: {value: changedValues.frequency / 1000},
             });
         wrapper
             .find('button')
             .simulate('submit');
-        expect(submitHandler).toHaveBeenCalledWith(changedValues)
+        expect(store.getActions()).toEqual([{
+            type: 'gameStart',
+            payload: changedValues
+        }])
     });
 
 });
