76d747be318b484f43213ac70ab43c8eef097e67
Viktoria Shirunova <vv16vv@users.noreply.github.com>
2021-01-26 14:27:57 +0300

    [hw09] use mockStore in stories for connected components

--- src/components/JumpingCellGame/JumpingCellGame.stories.tsx
+++ src/components/JumpingCellGame/JumpingCellGame.stories.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import {number, withKnobs} from "@storybook/addon-knobs";
+import {Provider} from "react-redux";
 
 import {JumpingCellGame} from "./JumpingCellGame";
 import {ErrorBoundary} from "@/components/ErrorBoundary";
+import {userLoggedIn} from "@/rdx/testConstants";
+import {mockStore} from "@/rdx/mockStore";
 
 export default {
     title: "Jumping Cell Game Story",
@@ -25,7 +28,22 @@ export const JumpingCellGameStory: React.FC<{}> = () => {
     const width = number("Width", 10, options);
     const height = number("Height", 10, options);
     const frequency = number("Frequency", 5000, optionsFreq);
+    const store = mockStore({
+        userReducer: userLoggedIn,
+        gameReducer: {
+            width,
+            height,
+            initFrequency: frequency,
+            currFrequency: frequency,
+            x: 0,
+            y: 0,
+            jumps: 0,
+            clicks: 0,
+        }
+    })
     return <ErrorBoundary>
-        <JumpingCellGame width={width} height={height} frequency={frequency}/>
+        <Provider store={store}>
+            <JumpingCellGame/>
+        </Provider>
     </ErrorBoundary>
 };
