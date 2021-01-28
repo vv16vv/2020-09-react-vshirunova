5452bd13e6c8418ae186c06d1cf24342d7e24a8b
Viktoria Shirunova <vv16vv@users.noreply.github.com>
2021-01-28 22:44:30 +0300

    [hw09] LogoutButton - use null instead of empty mapStateToProps

--- src/components/GameWithSettings/LogoutButton.tsx
+++ src/components/GameWithSettings/LogoutButton.tsx
@@ -4,7 +4,6 @@ import {connect} from "react-redux";
 
 import {StyledButton} from "@/components/styled/StyledComponents";
 import {clearName} from "@/rdx/user";
-import {AppState} from "@/rdx/reducers";
 
 interface ReduxProps {
     onLogout: () => void
@@ -17,14 +16,10 @@ const RawLogoutButton: React.FC<ReduxProps> = props => {
     return <StyledButton onClick={logout}>Log out</StyledButton>
 }
 
-function mapStateToProps(state: AppState) {
-    return {};
-}
-
 function mapDispatchToProps(dispatch: Dispatch) {
     return bindActionCreators({
         onLogout: clearName,
     }, dispatch)
 }
 
-export const LogoutButton = connect(mapStateToProps, mapDispatchToProps)(RawLogoutButton);
+export const LogoutButton = connect(null, mapDispatchToProps)(RawLogoutButton);
