0aa9143faaf79b7e03c963f8461e4d91f3008358
Viktoria Shirunova <vv16vv@users.noreply.github.com>
2021-02-08 19:52:00 +0300

    [hw11] add Saga middleware to mock store

--- src/rdx/mockStore.ts
+++ src/rdx/mockStore.ts
@@ -1,3 +1,7 @@
 import configureMockStore from "redux-mock-store";
+import createSagaMiddleware from "redux-saga";
+import {AppState} from "@/rdx/reducers";
 
-export const mockStore = configureMockStore()
+const sagaMiddleware = createSagaMiddleware()
+
+export const mockStore = configureMockStore<AppState>([sagaMiddleware])
